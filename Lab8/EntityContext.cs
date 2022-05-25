1675ffa8a1c0b793c43f1635a75b737dffa9a768
akshipulya <akshipulya@gmail.com>
2022-05-25 10:58:50 +0300

    initial commit

---
+++ Lab8/EntityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class EntityContext : DbContext
+    {
+        public EntityContext(string name) : base(name)
+        {
+            Database.SetInitializer(new DataBaseInitializer());
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Lab8/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EntityContext context;
+        public MainWindow()
+        {
+            context = new EntityContext("TestDbConnection");
+            InitializeComponent();
+            InitStudentList();
+        }
+        private void InitStudentList()
+        {
+            context.Students.Load();
+            dGrid.DataContext = context.Students.Local;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
